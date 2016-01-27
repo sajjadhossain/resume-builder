388ee57d343b3a10e33eb817e871f54bd0b422f9
sajjadhossain <sajjad@withpulp.com>
2016-01-27 03:23:08 -0500

    updated finals

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,1 +1,18 @@
 console.log('Welcome fellow developer or Web Inspector connoisseur.');
+document.addEventListener('DOMContentLoaded', function() {
+    // Smooth scroll https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+});
